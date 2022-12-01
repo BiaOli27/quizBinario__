5b29e563e95dc4ef7913845abd603dedd1e9a126
BiaOli27 <106164366+BiaOli27@users.noreply.github.com>
2022-12-01 18:52:52 -0300

    Add files via upload

---
+++ java/questions.js
@@ -0,0 +1,42 @@
+export default [
+    {
+      question: "Fazendo o processo inverso agora, transforme o número 1000111110 em sua forma decimal?",
+      answers: [
+        { option: "102", correct: true },
+        { option: "106", correct: false },
+        { option: "100", correct: false },
+      ],
+    },
+    {
+      question: "Os computadores utilizam o sistema binário ou de base 2 que é um sistema de numeração em que todas as quantidades se representam com base em dois números. Em um computador o número 2018, em base decimal, será representado, em base binária, por:",
+      answers: [
+        { option: "11111100000", correct: false },
+        { option: "11111100001", correct: true },
+        { option: "11111100001", correct: false },
+      ],
+    },
+    {
+      question: "Os números 500 e 1345 tem seus valores em binário, respectivamente iguais a:",
+      answers: [
+        { option: "101110100 e 10010000001", correct: false },
+        { option: "111110100 e 10101000000", correct: false },
+        { option: "111110100 e 10101000001", correct: true },
+      ],
+    },
+    {
+      question: "Binário é o adjetivo masculino que indica algo que tem duas unidades ou algo que é composto por dois elementos de informação. O sistema de numeração binário é usado em:",
+      answers: [
+        { option: "Várias áreas do conhecimento como Matemática,Física e Informática", correct: false },
+        { option: "Em computadores, mais não em notebooks", correct: false },
+        { option: "Binário é a linguagem em que você usa para conversar , usando 1 e 2", correct: true },
+      ],
+    },
+    {
+        question: "Converta os seguintes números de binário para dfecimal: 1100110 / 1100111, a soma desses números é igual a:",
+        answers: [
+          { option: "21", correct: false },
+          { option: "23", correct: false },
+          { option: "24", correct: true },
+        ],
+      },
+  ];
